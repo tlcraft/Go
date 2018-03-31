174d8a2023890bd6c26db1f960dbd7fe596d865e
tlcraft <tlcraft@users.noreply.github.com>
2018-03-30 23:26:17 -0600

    Create function type

--- First Steps/exercise_slices.go
+++ First Steps/exercise_slices.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"math"
-
 	"golang.org/x/tour/pic"
 )
+
+// Functions as parameters, define a type: https://stackoverflow.com/a/12655719/8094831
+type function func(x, y int) uint8
+
+var fn function
 
 func times(x, y int) uint8 {
 	return uint8(x * y)
 }
 
 func power(x, y int) uint8 {
-	return uint8(int(math.Pow(float64(x), float64(y))))
+	return uint8(x ^ y)
 }
 
 func parabola(x, y int) uint8 {
@@ -27,13 +30,17 @@ func Pic(dx, dy int) [][]uint8 {
 
 	for i := 0; i < dy; i++ {
 		for j := 0; j < dx; j++ {
-			pic[i][j] = parabola(j, i)
+			pic[i][j] = fn(i, j)
 		}
 	}
 
 	return pic
 }
 
+//Other solutions: https://gist.github.com/tetsuok/2280162
 func main() {
+	//fn = times
+	fn = power
+	//fn = parabola
 	pic.Show(Pic)
 }
