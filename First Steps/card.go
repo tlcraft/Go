4ac7d2585f582e284822e46e1a1c9c9842c3479b
tlcraft <tlcraft@users.noreply.github.com>
2018-04-05 19:27:05 -0600

    More fun with pointers

--- First Steps/card.go
+++ First Steps/card.go
@@ -14,6 +14,10 @@ func (c *Card) SetRank(newRank string) {
 	c.Rank = newRank
 }
 
+func SetRankFunc(c *Card, newRank string) {
+	c.Rank = newRank
+}
+
 func (c Card) Print() {
 	fmt.Println(c.Rank, c.Suit)
 }
@@ -22,17 +26,35 @@ func PrintFunc(c Card) {
 	fmt.Println(c.Rank, c.Suit)
 }
 
-func SetRankAlt(c *Card, newRank string) {
-	c.Rank = newRank
-}
-
 func main() {
-	var card = Card{"King", "Spades"}
-	card.SetSuit("Diamonds") // Won't change anything
-	card.SetRank("Jack")     // Correctly uses pointers
-	fmt.Println(card.Rank, card.Suit)
+	fmt.Println("Create two cards")
+	card, p := Card{"King", "Spades"}, &Card{"3", "Hearts"}
 
-	SetRankAlt(&card, "Ace") // Must be a pointer
+	card.Print()
+	p.Print() // Methods can use either a pointer or a value, pointers are interpreted in this case by Go as values (*p)
+	(*p).Print()
+	//*p.Print() // Error
+
+	fmt.Println("Alter the first card")
+	card.SetSuit("Diamonds") // Won't change anything, this method doesn't use pointers
+	card.SetRank("Jack")     // Go interprets the pointer &card for me
+	//&card.SetRank("Queen") // Error
+	(&card).SetRank("Queen")
 	card.Print()
 	PrintFunc(card)
+
+	fmt.Println("Alter the second card")
+	p.SetSuit("Clubs") // Still won't change anything
+	p.SetRank("10")    // Correctly uses pointers
+	p.Print()
+	PrintFunc(*p) // Needs the underlying value of the pointer
+
+	fmt.Println("Use a function to edit the rank")
+	SetRankFunc(&card, "Ace") // Must pass in a pointer
+	card.Print()
+	PrintFunc(card)
+
+	SetRankFunc(p, "5")
+	p.Print()
+	PrintFunc(*p) // Needs the underlying value of the pointer
 }
