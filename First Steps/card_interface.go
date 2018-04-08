2c40df3d114c19b26acecf4d93f97db0a7399bbd
tlcraft <tlcraft@users.noreply.github.com>
2018-04-08 01:23:23 -0600

    More interface practice

--- First Steps/card_interface.go
+++ First Steps/card_interface.go
@@ -7,6 +7,7 @@ import (
 
 type Card interface {
 	GetName() string
+	//GetRank() string // Causes an error because the concrete cards don't have a corresponding method - (missing GetRank method)
 }
 
 type StandardCard struct {
@@ -15,17 +16,42 @@ type StandardCard struct {
 }
 
 // Implicit Card interface implementation
-func (card StandardCard) GetName() string {
-	var b bytes.Buffer
+func (card *StandardCard) GetName() string {
+	if card == nil {
+		return "nil"
+	} else {
+		var b bytes.Buffer
 
-	b.WriteString(card.Rank)
-	b.WriteString(" of ")
-	b.WriteString(card.Suit)
+		b.WriteString(card.Rank)
+		b.WriteString(" of ")
+		b.WriteString(card.Suit)
 
-	return b.String()
+		return b.String()
+	}
+}
+
+type TCGCard struct {
+	Artist  string
+	Title   string
+	Defense int
+}
+
+func (tcg TCGCard) GetName() string {
+	return tcg.Title
 }
 
 func main() {
-	var c Card = StandardCard{"Ace", "Spades"}
+	var c Card = &StandardCard{"Ace", "Spades"}
 	fmt.Println(c.GetName())
+
+	var i Card = TCGCard{"Arty", "Goblin", 2}
+	fmt.Println(i.GetName())
+
+	var tcg TCGCard = TCGCard{"Art Jr", "Angel", 3}
+	fmt.Println(tcg.GetName())
+
+	var iCard Card
+	var nilCard *StandardCard
+	iCard = nilCard
+	fmt.Println(iCard.GetName())
 }
