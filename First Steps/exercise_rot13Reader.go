b91793ea282d7ded6c8c631d386903fc4301e994
tlcraft <tlcraft@users.noreply.github.com>
2018-04-22 19:37:14 -0600

    Update rot13 exercise
    Fixed my infinite loop

--- First Steps/exercise_rot13Reader.go
+++ First Steps/exercise_rot13Reader.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"fmt"
 	"io"
 	"os"
 	"strings"
@@ -14,32 +13,23 @@ type rot13Reader struct {
 // ROT13 Cipher: https://en.wikipedia.org/wiki/ROT13
 // Other solutions here: https://gist.github.com/flc/6439105
 func (rot13 rot13Reader) Read(b []byte) (int, error) {
-	bytes := make([]byte, 1)
-	var ind int = 0
+	bytes := make([]byte, len(b), cap(b))
+	n, err := rot13.r.Read(bytes)
 
-loop:
-	for {
-		n, err := rot13.r.Read(bytes)
-		//fmt.Printf("err=%v n=%v io.EOF=%v\n", err, n, io.EOF)
-		//fmt.Printf("b[]=%v", b)
-		if n == 0 || err == io.EOF {
-			break loop
-		}
-
-		if n > 0 && err == nil {
-			char := bytes[0]
+	if n > 0 && err == nil {
+		for i := 0; i < n; i++ {
+			char := bytes[i]
 			if char >= 'a' && char <= 'm' || char >= 'A' && char <= 'M' {
 				char += 13
 			} else if char > 'm' && char <= 'z' || char > 'M' && char <= 'Z' {
 				char -= 13
 			}
 
-			b[ind] = char
-			ind++
+			b[i] = char
 		}
 	}
 
-	return ind, nil
+	return n, err
 }
 
 func main() {
