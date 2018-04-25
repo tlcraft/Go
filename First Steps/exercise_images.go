02fe3501dcaad414f450c28dcaad3ee5f4d8581e
tlcraft <tlcraft@users.noreply.github.com>
2018-04-24 20:50:21 -0600

    Add my initial exercise-images.go solution

---
+++ First Steps/exercise_images.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct {
+	w int
+	h int
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, i.w, i.h)
+}
+
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x), uint8(y), 255, 255}
+}
+
+func main() {
+	m := Image{100, 150}
+	pic.ShowImage(m)
+}
