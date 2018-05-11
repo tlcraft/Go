a4d840e03b07285d3318f49b9647be3e0e2a80ab
tlcraft <tlcraft@users.noreply.github.com>
2018-05-10 19:55:00 -0600

    Refactor to a Character struct

--- First Steps/avengers.go
+++ First Steps/avengers.go
@@ -1,3 +1,9 @@
+// Resources
+//
+// A Tour of Go - https://tour.golang.org/
+// Inheritance in Go - https://hackthology.com/object-oriented-inheritance-in-go.html
+// crypto/rand - https://golang.org/pkg/crypto/rand/
+
 package main
 
 import (
@@ -29,50 +35,54 @@ func (m *SafeVillainMap) Contains(villain string) bool {
 }
 
 func main() {
-	fmt.Println("Heroes")
+	fmt.Println("** Heroes **")
 	for _, v := range heroes {
-		fmt.Printf("Name: %v, Attack Power: %v\n", v.name, v.attackPower)
+		fmt.Printf("Name: %v\n\tAttack Power: %v\n\tDefense: %v\n", v.name, v.attackPower, v.defense)
 	}
 
-	fmt.Println("Villains")
+	fmt.Println("-- Villains --")
 	for _, v := range villains {
-		fmt.Printf("Name: %v, Defense: %v\n", v.name, v.defense)
+		fmt.Printf("Name: %v\n\tAttack Power: %v\n\tDefense: %v\n", v.name, v.attackPower, v.defense)
 	}
 
 	// TODO
-	// have the heroes fight the villains concurrently (do attack power damage to defense)
-	// later incorporate advantages and disadvantages and teaming up against villains (villain "hero capacity")
-	// combine character attributes into one struct for reuse
+	// incorporate advantages and disadvantages for each character
+	// teaming up against villains (villain "hero capacity")
+	// special attacks and randomness for each battle
 }
 
-type avenger struct {
+func SaveTheWolrd() {
+	// TODO have the heroes fight the villains concurrently (do attack power damage to defense and vice versa)
+}
+
+type Character struct {
 	name        string
 	attackPower int
+	defense     int
 }
 
-var heroes = []avenger{
-	avenger{
+var heroes = []Character{
+	Character{
 		name:        "Thor",
 		attackPower: 20,
+		defense:     50,
 	},
-	avenger{
+	Character{
 		name:        "Iron Man",
 		attackPower: 15,
+		defense:     45,
 	},
 }
 
-type villain struct {
-	name    string
-	defense int
-}
-
-var villains = []villain{
-	villain{
-		name:    "Thanos",
-		defense: 100,
+var villains = []Character{
+	Character{
+		name:        "Thanos",
+		attackPower: 25,
+		defense:     100,
 	},
-	villain{
-		name:    "Ultron",
-		defense: 80,
+	Character{
+		name:        "Ultron",
+		attackPower: 15,
+		defense:     80,
 	},
 }
