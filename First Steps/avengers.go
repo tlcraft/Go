18e43d38811bcdf85f0e859bbd47d35c10fe9cb1
tlcraft <tlcraft@users.noreply.github.com>
2018-05-13 20:22:58 -0600

    Adding some initial logic to loop over villains

--- First Steps/avengers.go
+++ First Steps/avengers.go
@@ -44,6 +44,20 @@ func PrintStats(c []Character, header string) {
 func main() {
 	PrintStats(heroes, "** Heroes **")
 	PrintStats(villains, "-- Villains --")
+	numHeroes := len(heroes)
+	c := make([]chan string, numHeroes)
+
+	fmt.Println("FIGHT!\n")
+	for i, v := range heroes {
+		c[i] = make(chan string)
+		go SaveTheWolrd(i*len(villains)/numHeroes, (i+1)*len(villains)/numHeroes, v, c[i])
+	}
+
+	for i := range c {
+		for s := range c[i] {
+			fmt.Println(s)
+		}
+	}
 	// TODO
 	// incorporate advantages and disadvantages for each character
 	// teaming up against villains (villain "hero capacity")
@@ -51,10 +65,19 @@ func main() {
 
 	// Other Ideas
 	// Randomly add general enemies to a slice which the heroes iterate over and fight
+
+	fmt.Println("\nFinal Stats")
+	PrintStats(heroes, "** Heroes **")
+	PrintStats(villains, "-- Villains --")
 }
 
-func SaveTheWolrd() {
+func SaveTheWolrd(i, n int, hero Character, c chan string) {
+	defer close(c)
 	// TODO have the heroes fight the villains concurrently (do attack power damage to defense and vice versa)
+	for ; i < n; i++ {
+		villains[i].health -= hero.attackPower
+		c <- fmt.Sprintf("%v does %v damage to %v", hero.name, hero.attackPower, villains[i].name)
+	}
 
 	// while at least one hero is alive and enemies remain
 	// go fight(hero, enemy slice) --hero iterates over slice doing damage and taking damage
@@ -95,5 +118,23 @@ var villains = []Character{
 		attackPower: 15,
 		defense:     80,
 		health:      150,
+	},
+	Character{
+		name:        "Thug",
+		attackPower: 2,
+		defense:     10,
+		health:      20,
+	},
+	Character{
+		name:        "Criminal",
+		attackPower: 3,
+		defense:     8,
+		health:      15,
+	},
+	Character{
+		name:        "Goon",
+		attackPower: 31,
+		defense:     5,
+		health:      10,
 	},
 }
